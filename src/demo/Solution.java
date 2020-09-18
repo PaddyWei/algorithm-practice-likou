567c433343652399150e1ff39baaf651ab8de839
weipeng <598116879>
2020-09-18 10:45:42 +0800

    算法：全排列 II 给定一个可包含重复数字的序列，返回所有不重复的全排列。

--- src/demo/Solution.java
+++ src/demo/Solution.java
@@ -1697,8 +1697,59 @@ public class Solution {
         }
     }
 
+    public static List<List<Integer>> permuteUnique(int[] nums) {
+        int len = nums.length;
+        List<List<Integer>> res = new ArrayList<>();
+        if (len == 0) {
+            return res;
+        }
+
+        // 排序（升序或者降序都可以），排序是剪枝的前提
+        Arrays.sort(nums);
+
+        boolean[] used = new boolean[len];
+        // 使用 Deque 是 Java 官方 Stack 类的建议
+        Deque<Integer> path = new ArrayDeque<>(len);
+        dfs(nums, len, 0, used, path, res);
+        return res;
+    }
+
+    private static void dfs(int[] nums, int len, int depth, boolean[] used, Deque<Integer> path, List<List<Integer>> res) {
+        //栈内数量为总数数量，返回组合
+        if (depth == len) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int i = 0; i < len; ++i) {
+            if (used[i]) {
+                continue;
+            }
+
+            // 剪枝条件：i > 0 是为了保证 nums[i - 1] 有意义
+            // 写 !used[i - 1] 是因为 nums[i - 1] 在深度优先遍历的过程中刚刚被撤销选择
+            /*
+                剪枝:
+                    1、当前元素 被使用过
+                    2、当前元素 和 前一个元素相同(排序保证相同元素排在一起)，
+                        且 前一个元素 没有被使用过(保证重复元素只按照一个顺序被遍历，防止 重复排列)
+             */
+            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) {
+                continue;
+            }
+
+            path.addLast(nums[i]);
+            used[i] = true;
+
+            dfs(nums, len, depth + 1, used, path, res);
+            // 回溯部分的代码，和 dfs 之前的代码是对称的
+            used[i] = false;
+            path.removeLast();
+        }
+    }
+
     public static void main(String[] args) throws IOException {
-        System.out.println();
+        System.out.println(permuteUnique(new int[]{1,2,3}));
     }
 
 }
