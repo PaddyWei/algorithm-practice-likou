30c3d06419b25e6dbbd6796a80976f94c8b58389
weipeng <598116879>
2020-09-22 09:48:58 +0800

    算法：监控二叉树 给定一个二叉树，我们在树的节点上安装摄像头。 节点上的每个摄影头都可以监视其父对象、自身及其直接子对象。 计算监控树的所有节点所需的最小摄像头数量。

--- src/demo/Solution.java
+++ src/demo/Solution.java
@@ -1800,6 +1800,34 @@ public class Solution {
         return b;
     }
 
+    /**
+     * 监控二叉树
+     */
+    int resultNum = 0;
+    public int minCameraCover(TreeNode root) {
+        if(dfs(root)==1){
+            resultNum++;
+        }
+        return resultNum;
+    }
+    //0:可被观测但无监控，上一层节点为1
+    //1：不可被观测到，上一层节点为2
+    //2：有摄像机，上一层节点为0
+    private int dfs(TreeNode root){
+        if(root==null){
+            return 0;
+        }
+        int leftStatus = dfs(root.left),rightStatus = dfs(root.right);
+        if(leftStatus==1||rightStatus==1){
+            resultNum++;
+            return 2;
+        }else if(leftStatus==2||rightStatus==2){
+            return 0;
+        }else{
+            return 1;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         System.out.println(exist(new char[][]{{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}}, "SEE"));
     }
