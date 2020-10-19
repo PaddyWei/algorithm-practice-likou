b2c265a910ab0a7924f74c2bf1380d7bd7402bca
weipeng <598116879>
2020-10-19 15:19:38 +0800

    算法：有序数组的平方 给定一个按非递减顺序排序的整数数组 A，返回每个数字的平方组成的新数组，要求也按非递减顺序排序。
    示例 1：
    
    输入：[-4,-1,0,3,10]
    输出：[0,1,9,16,100]
    
    示例 2：
    
    输入：[-7,-3,2,3,11]
    输出：[4,9,9,49,121]

--- src/demo/Solution.java
+++ src/demo/Solution.java
@@ -2476,6 +2476,20 @@ public class Solution {
         dfs(curr.getRight(), curr.getNext() == null ? null : curr.getNext().getRight());
     }
 
+    /**
+     * 有序数组的平方
+     *
+     * @param A
+     * @return
+     */
+    public int[] sortedSquares(int[] A) {
+        for (int i = 0; i < A.length; i++) {
+            A[i] *= A[i];
+        }
+        Arrays.sort(A);
+        return A;
+    }
+
     public static void main(String[] args) throws IOException {
         System.out.println(commonChars2(new String[]{"bella", "label", "roller"}));
     }
