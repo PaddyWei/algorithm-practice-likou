9051bb9b30f3cd2584ce1e7a3ff2887d84251daf
weipeng <598116879>
2020-11-25 15:32:43 +0800

    算法：上升下降字符串
    给你一个字符串 s ，请你根据下面的算法重新构造字符串：
    
    1、从 s 中选出 最小 的字符，将它 接在 结果字符串的后面。
    2、从 s 剩余字符中选出 最小 的字符，且该字符比上一个添加的字符大，将它 接在 结果字符串后面。
    3、重复步骤 2 ，直到你没法从 s 中选择字符。
    4、从 s 中选出 最大 的字符，将它 接在 结果字符串的后面。
    5、从 s 剩余字符中选出 最大 的字符，且该字符比上一个添加的字符小，将它 接在 结果字符串后面。
    6、重复步骤 5 ，直到你没法从 s 中选择字符。
    7、重复步骤 1 到 6 ，直到 s 中所有字符都已经被选过。
    在任何一步中，如果最小或者最大字符不止一个 ，你可以选择其中任意一个，并将其添加到结果字符串。
    
    请你返回将 s 中字符重新排序后的 结果字符串 。

--- src/demo/Utils.java
+++ src/demo/Utils.java
@@ -1,5 +1,8 @@
 package demo;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * @ClassName : Utils
  * @Description : Utils
@@ -29,6 +32,48 @@ public class Utils {
             output[index] = Integer.parseInt(part);
         }
         return output;
+    }
+
+    /**
+     * 合并时间段 -- 针对一天二十四小时的时间
+     * @param collect ["11:30:00-12:30:00","13:30:00-14:30:00","15:30:00-16:30:00"]
+     * @return
+     */
+    public static List<String> mergeTimeByDay(List<String> collect){
+        if (collect == null || collect.size() == 0) {
+            return null;
+        }
+        //对时间进行先后排序
+        collect = collect.stream().sorted().collect(Collectors.toList());
+        for (int i = 0; i < collect.size() - 1; ) {
+            String[] split = collect.get(i).split("-");
+            String beforeStartTime = split[0];
+            String beforeEndTime = split[1];
+            String[] oldSplit = collect.get(i + 1).split("-");
+            String afterStartTime = oldSplit[0];
+            String afterEndTime = oldSplit[1];
+            if (beforeStartTime.compareTo(afterEndTime) > 0 || beforeEndTime.compareTo(afterStartTime) < 0) {
+                i++;
+                continue;
+            }
+            int i1 = beforeStartTime.compareTo(afterStartTime);
+            int i2 = beforeStartTime.compareTo(afterEndTime);
+            if (afterStartTime.compareTo(beforeStartTime) >= 0 && afterStartTime.compareTo(beforeEndTime) <= 0) {
+                afterStartTime = beforeStartTime.compareTo(afterStartTime) < 0 ? beforeStartTime : afterStartTime;
+                afterEndTime = beforeEndTime.compareTo(afterEndTime) > 0 ? beforeEndTime : afterEndTime;
+            }
+            int i3 = beforeEndTime.compareTo(afterStartTime);
+            int i4 = beforeEndTime.compareTo(afterEndTime);
+            if (afterEndTime.compareTo(beforeStartTime) <= 0 && afterEndTime.compareTo(beforeEndTime) >= 0) {
+                afterStartTime = beforeStartTime.compareTo(afterStartTime) < 0 ? beforeStartTime : afterStartTime;
+                afterEndTime = beforeEndTime.compareTo(afterEndTime) > 0 ? beforeEndTime : afterEndTime;
+            }
+            String newTime = afterStartTime + "-" + afterEndTime;
+            collect.remove(i);
+            collect.set(i, newTime);
+        }
+
+        return collect;
     }
 
 }
